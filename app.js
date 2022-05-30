aa3177beec1e94e6a1256f396b3ae2a0bc8802f1
therealofarah <omaarfaarah2019@gmail.com>
2022-05-30 16:17:19 -0400

    complete task 5

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("Omars List")
+// console.log("Omars List")
+const btn = document.getElementById("#submit-button")
+const inpt = document.getElementById("list-items")
+const ul = document.getElementById("todo-list")
+
