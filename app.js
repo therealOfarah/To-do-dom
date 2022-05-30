b445244a99f569485551bec3a029391fb8fa35ea
therealofarah <omaarfaarah2019@gmail.com>
2022-05-30 18:17:14 -0400

    stop empty list

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ btn.addEventListener("click" , function(evt){
     const li = document.createElement('li')
     const inpt = document.getElementById("list-items")
     li.textContent = inpt.value 
+    if(li.textContent === ""){
+      
+    }
+    else{
       document.querySelector("ul").appendChild(li)
+    }
     inpt.value=""
     // console.log(inpt.value)
     // if (li === ""){
