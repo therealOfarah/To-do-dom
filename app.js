628d48fc82c8b914dec174101a8a33a8678c883c
therealofarah <omaarfaarah2019@gmail.com>
2022-05-30 18:02:29 -0400

    complete task 6

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 // console.log("Omars List")
-const btn = document.getElementById("#submit-button")
-const inpt = document.getElementById("list-items")
+const btn = document.getElementById("submit-button")
+// const inpt = document.getElementById("list-items")
 const ul = document.getElementById("todo-list")
+console.log(btn)
+btn.addEventListener("click" , function(evt){
+    const li = document.createElement('li')
+    const inpt = document.getElementById("list-items")
+    li.textContent = inpt.value
+    document.querySelector("ul").appendChild(li)
+    inpt.value=""
+    console.log(inpt.value)
 
+  });
